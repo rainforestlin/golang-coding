52103a59f14d71ab989656ab576cef5c923b86f6
李俊霖 <julianlee107@hotmail.com>
2019-12-23 18:40:00 +0800

    新增正则表达式测试

--- regularExpression/main.go
+++ regularExpression/main.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"golang-coding/regularExpression/usualRegularExpression"
-)
-func main()  {
-	anyNumber := usualRegularExpression.RegularExpression{"\\d+","simpleInt","任意数字"}
-	result:=anyNumber.VerifyExpression("adkjflsfj456")
-	fmt.Println(result)
-	nNumber := usualRegularExpression.RegularExpression{"\\d{2}","simpleInt","N位数字"}
-	result = nNumber.VerifyExpression("xdi234dll45")
-	fmt.Print(result)
-	
+import "fmt"
+
+func main() {
+fmt.Printf("d")
 }
