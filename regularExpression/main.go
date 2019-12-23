07b4d8a190d0322e06b9fd401336bc8dd8a973c8
李俊霖 <julianlee107@hotmail.com>
2019-12-23 16:21:27 +0800

    正则表达式

--- regularExpression/main.go
+++ regularExpression/main.go
@@ -1,1 +1,15 @@
-package regularExpression
+package main
+
+import (
+	"fmt"
+	"golang-coding/regularExpression/usualRegularExpression"
+)
+func main()  {
+	anyNumber := usualRegularExpression.RegularExpression{"\\d+","simpleInt","任意数字"}
+	result:=anyNumber.VerifyExpression("adkjflsfj456")
+	fmt.Println(result)
+	nNumber := usualRegularExpression.RegularExpression{"\\d{2}","simpleInt","N位数字"}
+	result = nNumber.VerifyExpression("xdi234dll45")
+	fmt.Print(result)
+	
+}
--- regularExpression/usualRegularExpression/regularExpression.go
+++ regularExpression/usualRegularExpression/regularExpression.go
@@ -1,1 +1,27 @@
 package usualRegularExpression
+
+import (
+	"fmt"
+	"regexp"
+)
+
+type RegularExpression struct {
+	Expression string
+	Type       string
+	Annotation string
+}
+
+func (re RegularExpression) VerifyExpression(str string) interface{} {
+	compile, e := regexp.Compile(re.Expression)
+	if e != nil {
+		panic(e)
+	}
+	if re.Type == "simpleInt" {
+		match := compile.FindAllString(str, -1)
+		fmt.Printf("method:%s -----%s", re.Annotation, match)
+		fmt.Println()
+		return match
+	}
+	return ""
+
+}
