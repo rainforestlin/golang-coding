93a1976cdd1523c763659d8bc1e4d5c3814bcfde
李俊霖 <julianlee107@hotmail.com>
2019-12-23 14:36:20 +0800

    针对网站charset不为utf-8时进行转码

---
+++ webPageEncoding/go.mod
@@ -0,0 +1,8 @@
+module golang-coding/webPageEncoding
+
+go 1.13
+
+require (
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
--- webPageEncoding/main.go
+++ webPageEncoding/main.go
@@ -1,1 +1,46 @@
-package webPageEncoding
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+// 使用http对网页进行解析时，当中文字符不为utf-8格式时，中文显示乱码
+// when I was using http to parse html, can't display simplified chinese aright while charset is gbk
+func main() {
+	resp, err := http.Get("https://www.lagou.com/")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusOK {
+		encodeBody := determineEncoding(resp.Body)
+		utf8Reader := transform.NewReader(resp.Body, encodeBody.NewDecoder())
+		all, err := ioutil.ReadAll(utf8Reader)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s\n", all)
+
+	}
+
+}
+
+func determineEncoding(r io.Reader) encoding.Encoding {
+	//读取前面1024个字节
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	//判断编码
+	encode, name, _ := charset.DetermineEncoding(bytes, "")
+	// 为编码类型
+	fmt.Print(name)
+	return encode
+}
