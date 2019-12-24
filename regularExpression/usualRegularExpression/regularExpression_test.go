d8e4d011b841121a4166e1fbdff71369289e423d
李俊霖 <julianlee107@hotmail.com>
2019-12-24 11:27:54 +0800

    新增基准测试

--- regularExpression/usualRegularExpression/regularExpression_test.go
+++ regularExpression/usualRegularExpression/regularExpression_test.go
@@ -12,16 +12,14 @@ func TestVerify(t *testing.T) {
 		ran          []string
 		expected     []string
 	}{
-		{"AnyNumbers", "任意数字", "adadsjkl123", []string{},[]string{"123"}},
-		{"nDigitalNumbers", "N位数字", "dfjk14234sklfjl12fjewe3456778", []string{"3", "5"},[]string{"14234","34567"}},
-		{"chinese", "中文", "my name is Julian Lee, chinese name 李俊霖", []string{},[]string{"李俊霖"}},
-
-
+		{"AnyNumbers", "任意数字", "adadsjkl123", []string{}, []string{"123"}},
+		{"nDigitalNumbers", "N位数字", "dfjk14234sklfjl12fjewe3456778", []string{"3", "5"}, []string{"14234", "34567"}},
+		{"chinese", "中文", "my name is Julian Lee, chinese name 李俊霖", []string{}, []string{"李俊霖"}},
 	}
 	for _, test := range regularExpressionTest {
 		actual := Verify(test.types, test.annotation, test.verifyString, test.ran)
-		if !StringSliceEqual(actual,test.expected){
-			t.Errorf("Verfiy(%s) according to %s;get %s but expected %s",test.verifyString,test.annotation,actual,test.expected)
+		if !StringSliceEqual(actual, test.expected) {
+			t.Errorf("Verfiy(%s) according to %s;get %s but expected %s", test.verifyString, test.annotation, actual, test.expected)
 		}
 	}
 }
