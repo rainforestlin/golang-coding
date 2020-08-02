80a469b794370d91d124761978df3fb8570b17e0
Julian Lee <julianlee107@hotmail.com>
2020-08-02 16:12:54 +0800

    add how go read from a file

---
+++ fileopen/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	readFromFileByIO()
+}
+
+func readFromFile() {
+	fileObj, err := os.Open("main.go")
+	if err != nil {
+		fmt.Printf("open file err:%v \n", err)
+	}
+	defer fileObj.Close()
+	var temp = make([]byte, 1000)
+	n, err := fileObj.Read(temp)
+	if err != nil {
+		fmt.Printf("read from file err:%v", err)
+	}
+	fmt.Printf("read byte %d \n", n)
+	fmt.Printf("read from file %s", string(temp[:]))
+}
+
+func readFromFileByBuff() {
+	fileObj, err := os.Open("main.go")
+	if err != nil {
+		fmt.Printf("open file err:%v \n", err)
+	}
+	defer fileObj.Close()
+
+	reader := bufio.NewReader(fileObj)
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			fmt.Printf("%v", err)
+			return
+		}
+		if err != nil {
+			fmt.Printf("%v", err)
+			return
+		}
+		fmt.Print(line)
+	}
+
+}
+
+func readFromFileByIO() {
+	ret, err := ioutil.ReadFile("./main.go")
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+	fmt.Println(ret)
+}
